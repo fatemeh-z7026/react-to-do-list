d955ffb1775f8209c85bdbb700c82fa6acc6aa67
F-zeinali <fatemeh.zeinali7026@gmail.com>
2024-10-27 11:48:28 +0330

    install react bootstrap

---
+++ to-do-list/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import TodoList from "./components/ToDoList/ToDoList";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList></TodoList>
+    </div>
+  );
+}
+
+export default App;
